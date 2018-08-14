a3c70ab3b74a293cb9321cfbd27a6f6e54e10ae7
Gregorio1809 <gregorioimanuel@gmail.com>
2018-08-14 15:20:52 +0700

    sampai di poin 11

--- app/src/main/java/com/example/gregorio/mynotesapp/db/DatabaseContract.java
+++ app/src/main/java/com/example/gregorio/mynotesapp/db/DatabaseContract.java
@@ -1,13 +1,35 @@
 package com.example.gregorio.mynotesapp.db;
 
+import android.database.Cursor;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public class DatabaseContract {
-    static String TABLE_NAME = "note";
-    static final class NoteColumns implements BaseColumns{
+    public static String TABLE_NAME = "note";
+
+    static final class NoteColumns implements BaseColumns {
         static String TITLE = "title";
         static String DESCRIPTION = "description";
         static String DATE = "date";
+    }
+
+    public static final String AUTHORITY = "com.example.gregorio.mynotesapp";
+
+    public static final Uri CONTENT_URI = new Uri.Builder().scheme("content")
+            .authority(AUTHORITY)
+            .appendPath(TABLE_NAME)
+            .build();
+
+    public static String getColumnString(Cursor cursor, String columnName){
+        return cursor.getString(cursor.getColumnIndex(columnName));
+    }
+
+    public static int getColumnInt(Cursor cursor, String columnName){
+        return cursor.getInt(cursor.getColumnIndex(columnName));
+    }
+
+    public static long getColumnLong(Cursor cursor, String columnName){
+        return cursor.getLong(cursor.getColumnIndex(columnName));
     }
 
 }
